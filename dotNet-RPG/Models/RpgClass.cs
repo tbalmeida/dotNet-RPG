f93ddb75a32c34739556aabc92f100b3fd214355
Thiago Almeida <tbalmeida@gmail.com>
2020-11-10 14:24:43 -0700

    Added Models/Character and classes

---
+++ dotNet-RPG/Models/RpgClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNet_RPG.Models
+{
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3,
+        Rogue = 4,
+        Healer = 5
+    }
+}
