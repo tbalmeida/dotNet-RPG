1d54943c4d9fb70e763441de1e9449c108b00f68
Thiago Almeida <tbalmeida@gmail.com>
2020-11-10 15:29:11 -0700

    Added controler and 1 action

---
+++ dotNet-RPG/Controllers/CharacterController.cs
@@ -0,0 +1,17 @@
+﻿using dotNet_RPG.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNet_RPG.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase   // without view support
+    {
+        private static Character knight = new Character();
+
+        public IActionResult Get()
+        {
+            return Ok(knight);
+        }
+    }
+}
