42ea8ea7e1264f88bf8795e9eb315af1c358438d
Thiago Almeida <tbalmeida@gmail.com>
2020-11-12 22:10:52 -0700

    POST new character on db

--- dotNet-RPG/Service/CharacterService/CharacterService.cs
+++ dotNet-RPG/Service/CharacterService/CharacterService.cs
@@ -33,9 +33,9 @@ namespace dotNet_RPG.Service.CharacterService
         {
             ServiceResponse<List<GetCharacterDTO>> serviceResponse = new ServiceResponse<List<GetCharacterDTO>>();
             Character character = _mapper.Map<Character>(newCharacter);
-            character.Id = characters.Max(c => c.Id) + 1;
-            characters.Add(character);
-            serviceResponse.Data = (characters.Select(c => _mapper.Map<GetCharacterDTO>(c))).ToList();
+            await _context.Characters.AddAsync(character);
+            await _context.SaveChangesAsync();
+            serviceResponse.Data = (_context.Characters.Select(c => _mapper.Map<GetCharacterDTO>(c))).ToList();
             return serviceResponse;
         }
 
