8a7d0f4ed94e601eb23988c5fb59cd1e0998b3ec
Thiago Almeida <tbalmeida@gmail.com>
2020-11-14 08:56:26 -0700

    Starting auth

---
+++ dotNet-RPG/Data/AuthRepository.cs
@@ -0,0 +1,28 @@
+﻿using dotNet_RPG.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNet_RPG.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+
+        public Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
---
+++ dotNet-RPG/Data/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using dotNet_RPG.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNet_RPG.Data
+{
+    public class IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+
+        Task<ServiceResponse<string>> Login(string username, string password);
+
+        Task<bool> UserExists(string username);
+}
